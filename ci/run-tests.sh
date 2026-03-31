d8919d95d9550f709a69dd2e9509669a6631b439
ebn-harness <eric.brown@harness.io>
2026-03-31 13:32:51 -0700

    test: Add unit tests for JFR init container template

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -24,6 +24,7 @@ run_scenario "PDB"           "${VALUES_DIR}/pdb.yaml"
 run_scenario "KEDA"          "${VALUES_DIR}/keda.yaml"
 run_scenario "Ingress"       "${VALUES_DIR}/ingress.yaml"
 run_scenario "VirtualService" "${VALUES_DIR}/virtualservice.yaml"
+run_scenario "JFR"            "${VALUES_DIR}/jfr.yaml"
 echo "All template scenarios passed."
 
 echo ""
